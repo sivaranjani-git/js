28d4d29c034fe679bad4347d473600df5550125f
sivaranjani-git <134845313+sivaranjani-git@users.noreply.github.com>
2023-08-15 12:23:01 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,35 @@
+//IMPLICIT CONVENSION
+
+//string +anydatatype ====string
+
+  let num1 ="5";
+  let num2 =2;
+  console.log(num1+num2);
+  
+  let num3 = "5";
+  let num4 = true;
+  console.log(num3 +  num4);
+
+  let num5 = "5";
+  let num6 = undefined;
+  console.log(num5 + num6);
+ 
+  let num7 = "5";
+  let num8 = null;
+  console.log(num7 + num8); 
+
+  //numeric string -/ *
+
+  let a= "7";
+  let b= "4";
+  console.log(a - b);
+  console.log(a * b);
+  console.log(a / b);
+
+  //non numeric string
+
+  let aa="abc";
+  let bb="2";
+  console.log(aa - bb);
+
+  
